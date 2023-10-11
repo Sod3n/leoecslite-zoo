dd17c7ec66eafee90b86c81b8743500a4c5b4614
Nenpoto <59008922+Sod3n@users.noreply.github.com>
2023-10-11 10:31:39 +0700

    FixLast
    UnchangeDelHere
    
    FixDelHere
    
    AddDelHereEntityWith
    
    Fix2

--- Scripts/DelHere/DelHereEntitySystem.cs
+++ Scripts/DelHere/DelHereEntitySystem.cs
@@ -2,16 +2,15 @@ using Leopotam.EcsLite;
 
 namespace AleVerDes.LeoEcsLiteZoo
 {
-    public sealed class DelHereSystem<T> : IEcsRunSystem where T : struct
+    public sealed class DelHereEntitySystem<T> : IEcsRunSystem where T : struct
     {
         public void Run(IEcsSystems systems)
         {
             var world = systems.GetWorld();
             var filter = world.Filter<T>().End();
-            var pool = world.GetPool<T>();
             foreach (var entity in filter)
             {
-                pool.Del(entity);
+                world.DelEntity(entity);
             }
         }
     }
